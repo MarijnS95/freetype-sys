b6c2299a2c7e49f2a94eedb0689e73f638361d42
Marijn Suijten <marijn@traverseresearch.nl>
2022-08-09 18:00:01 +0200

    Disable cmake by default
    Not everyone apprecates cmake support and rather uses system libraries
    by default instead of pulling in `cmake` into their dependency tree.
    
    https://github.com/rust-windowing/winit/issues/ 2373

--- build.rs
+++ build.rs
@@ -1,7 +1,3 @@
-extern crate cmake;
-extern crate pkg_config;
-
-use cmake::Config;
 use std::env;
 
 fn main() {
@@ -9,24 +5,31 @@ fn main() {
     if !target.contains("android")
         && pkg_config::Config::new().atleast_version("18.5.12").find("freetype2").is_ok()
     {
-        return
+        return;
     }
 
-    let mut config = Config::new("freetype2");
-    if let Ok(s) = env::var("FREETYPE_CMAKE_GENERATOR") {
-        config.generator(s);
+    #[cfg(feature = "cmake")]
+    {
+        let mut config = cmake::Config::new("freetype2");
+        if let Ok(s) = env::var("FREETYPE_CMAKE_GENERATOR") {
+            config.generator(s);
+        }
+        let dst = config
+            .define("CMAKE_INSTALL_LIBDIR", "lib")
+            .define("CMAKE_DISABLE_FIND_PACKAGE_BZip2", "TRUE")
+            .define("CMAKE_DISABLE_FIND_PACKAGE_HarfBuzz", "TRUE")
+            .define("CMAKE_DISABLE_FIND_PACKAGE_PNG", "TRUE")
+            .define("CMAKE_DISABLE_FIND_PACKAGE_ZLIB", "TRUE")
+            .define("CMAKE_DISABLE_FIND_PACKAGE_BrotliDec", "TRUE")
+            .profile("Release")
+            .build();
+        let out_dir = env::var("OUT_DIR").unwrap();
+        println!("cargo:rustc-link-search=native={}/lib", dst.display());
+        println!("cargo:rustc-link-lib=static=freetype");
+        println!("cargo:outdir={}", out_dir);
+
+        return;
     }
-    let dst = config
-        .define("CMAKE_INSTALL_LIBDIR", "lib")
-        .define("CMAKE_DISABLE_FIND_PACKAGE_BZip2", "TRUE")
-        .define("CMAKE_DISABLE_FIND_PACKAGE_HarfBuzz", "TRUE")
-        .define("CMAKE_DISABLE_FIND_PACKAGE_PNG", "TRUE")
-        .define("CMAKE_DISABLE_FIND_PACKAGE_ZLIB", "TRUE")
-        .define("CMAKE_DISABLE_FIND_PACKAGE_BrotliDec", "TRUE")
-        .profile("Release")
-        .build();
-    let out_dir = env::var("OUT_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static=freetype");
-    println!("cargo:outdir={}", out_dir);
+
+    panic!("`freetype2` not found using pkg-config and `cmake` is disabled");
 }
